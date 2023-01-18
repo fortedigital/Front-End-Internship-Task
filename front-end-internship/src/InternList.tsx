1c6c22f98ad54265ee3ed8ffbfcc5ca7a0d071a6
Mateusz <marchewka.matthew@gmail.com>
2023-01-18 12:44:57 +0100

    Fix Intern list

--- front-end-internship/src/InternList.tsx
+++ front-end-internship/src/InternList.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
+interface Intern {
+  id: number;
+  name: string;
+}
+
 export const InternList: React.FC = () => {
-  const [interns, setInterns] = useState([]);
+  const [interns, setInterns] = useState<Intern[]>([]);
 
   useEffect(() => {
       const fetchInterns = async () => {
           const response = await fetch('http://localhost:3001/interns');
-          const interns = await response.json();
+          const interns: Intern[] = await response.json();
           setInterns(interns);
       }
       fetchInterns();
@@ -15,8 +20,11 @@ export const InternList: React.FC = () => {
 
   return (
     <div>
-      Interns list
-      {/* {interns.map(intern => (<div key={intern.id}>{intern.name} <NavLink to={`/interns/${intern.id}`}>Edit</NavLink></div>))} */}
+      {interns.map(intern => (
+        <div key={intern.id}>
+          {intern.name} <NavLink to={`/interns/${intern.id}`}>Edit</NavLink>
+        </div>
+      ))}
     </div>
   )
 }
