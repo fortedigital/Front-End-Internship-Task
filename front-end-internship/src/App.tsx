171bdb0352818bc5b010e1c6031e74b6e0f8758e
Mateusz <marchewka.matthew@gmail.com>
2023-01-18 12:30:37 +0100

    fix test

--- front-end-internship/src/App.tsx
+++ front-end-internship/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { InternList } from './InternList';
 import { EditIntern } from './EditIntern';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<InternList />} />
-        <Route path="/interns/:id" element={<EditIntern />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<InternList />} />
+          <Route path="/interns/:id" element={<EditIntern />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
